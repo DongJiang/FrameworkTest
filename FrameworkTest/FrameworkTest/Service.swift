17293334ae0e411483e167fbbcbf11c776572cf8
Jiang Dong <jiang.dong@aalto.fi>
2019-01-07 09:34:31 +0200

    added a test framework

---
+++ FrameworkTest/FrameworkTest/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  FrameworkTest
+//
+//  Created by Jiang Dong on 07/01/2019.
+//  Copyright © 2019 vimai. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    private init(){
+        
+    }
+    
+    public static func getTestResult() -> String{
+        return "Test success!"
+    }
+}
